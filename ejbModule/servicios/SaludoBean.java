09a71450de0c3abff8e4bfcf5b856b3b7ed7a8aa
Santiago Da Silva <sdasilva@outlook.es>
2020-10-06 21:56:28 -0300

    EJB SaludoBean sin estado remoto

---
+++ ejbModule/servicios/SaludoBean.java
@@ -0,0 +1,18 @@
+package servicios;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class SaludoBean
+ */
+@Stateless
+public class SaludoBean implements SaludoBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public SaludoBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ ejbModule/servicios/SaludoBeanRemote.java
@@ -0,0 +1,8 @@
+package servicios;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface SaludoBeanRemote {
+
+}
